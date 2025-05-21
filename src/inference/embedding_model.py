47f293264f1cebf5236995390a03883b51125487
Kim, Taehoon <taehoonkim@sogang.ac.kr>
2025-05-21 22:53:27 +0900

    Update embedding_model.py

--- src/inference/embedding_model.py
+++ src/inference/embedding_model.py
@@ -35,4 +35,4 @@ elif torch.backends.mps.is_available():
     device = "mps"
 else:
     device = "cpu"
-embedding_model = EmbeddingModel(model_name="nlpai-lab/KURE-v1", device=device)
+embedding_model = EmbeddingModel(model_name="BAAI/bge-m3", device=device)
